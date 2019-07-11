1bfcdad17c8064f4e91fb029429cab144aa1a177
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-07-11 06:28:27 +0200

    added emacs' dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((chicken-mode
+  . ((eval . (put 'case-token			'scheme-indent-function 1))
+     )))
+
+;;; end of file
